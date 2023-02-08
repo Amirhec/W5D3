4bc088676a5c0c46a076591aa3ff283c45bd8675
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-02-08 11:20:56 -0500

    Figuring out tables

--- import_db.sql
+++ import_db.sql
@@ -1,19 +1,30 @@
-CREATE TABLE users (
+CREATE TABLE users ( --users are authors, both original asker and replier
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 );
 
-CREATE TABLE questions (
-    title TEXT,
-    body TEXT
+CREATE TABLE questions ( --being asked by users/authors
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER PRIMARY KEY,
 
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
-    inner join questions
-)
+    
+    FOREIGN KEY(xx) REFERENCES users(id),
+    FOREIGN KEY(xx) REFERENCES questions(id)
+    -- inner join questions & users
+);
 
+CREATE TABLE replies ( --original question, user(student AND/OR teacher)
+  id INTEGER PRIMARY KEY,
+-- Reply::find_by_user_id(user_id)
+-- Reply::find_by_question_id(question_id)
+);
 
-
+CREATE TABLE questions_like (
+);
